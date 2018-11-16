78010ce1f484e1670c72ae48fefb5e9ce3e0ab9d
nicoleburke <nicole.b.burke@gmail.com>
2018-11-15 23:52:06 -0600

    trying to make tidyverse load automatically

--- R/getridofNA.R
+++ R/getridofNA.R
@@ -1,14 +1,12 @@
 #### getridofNA function
 #### aoianalysis package
-#### Created by Nicole Burke 
-#### 08/2018-11/2018 
+#### Created by Nicole Burke
+#### 08/2018-11/2018
 
 #### What does function do? #####
 # Takes 2 desired AOI columns and gets rid of NAs. It replaces NAs with 999.
-
-library(tidyverse)
-
 getridofNA_help <- function(dataframe, AOI1, AOI2) {
+  library(tidyverse)
   dataframe <- dataframe %>%
     # Take columns and replace NAs with 999
     mutate(AOI1 = ifelse(is.na(AOI1), 999, AOI1),
